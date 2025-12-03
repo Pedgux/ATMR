20759bd8d69f35786421bb741321d883e74c57e1
matulehti <matiaslehtinen08@gmail.com>
2025-12-03 15:13:06 +0200

    ui shiat

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@ using Spectre.Console;
 
 public static class Program
 {
-    public static async Task Aloitus(string lobbyCode)
+    public static async Task Initialize(string lobbyCode)
     {
         Input.Poll();
         await UdpTransport.Initialize(lobbyCode);
@@ -19,8 +19,23 @@ public static class Program
 
     public static async Task Main(string[] args)
     {
-        var lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
-        await Aloitus(lobbyCode);
+        // Create a table
+        var table = new Table()
+            .AddColumn("ID")
+            .AddColumn("Methods")
+            .AddColumn("Purpose")
+            .AddRow("1", "Center()", "Initializes a new instance that is center aligned")
+            .AddRow("2", "Measure()", "Measures the renderable object")
+            .AddRow("3", "Right()", "Initializes a new instance that is right aligned.");
+
+        // Create a panel
+        var panel = new Panel(table).Header("Other Align Methods").Border(BoxBorder.Double);
+
+        // Renders the panel in the top-center of the console
+        AnsiConsole.Write(new Align(panel, HorizontalAlignment.Center, VerticalAlignment.Top));
+
+        //var lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
+        //await Initialize(lobbyCode);
     }
 
     // placeholder ig
