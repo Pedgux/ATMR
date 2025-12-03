cb2b3c7f975708cc248f0fa8cc805f9c9da3a220
matulehti <matiaslehtinen08@gmail.com>
2025-12-03 10:54:52 +0200

    ????

--- src/Program.cs
+++ src/Program.cs
@@ -4,32 +4,15 @@ Inspired by Nathan Daniel's "Roguelike Theory of Relativity (RTOR)" paper
 Built with Spectre.Console for console UI: https://spectreconsole.net/
 */
 
-using System.Threading.Tasks;
 using ATMR.Networking;
 using Spectre.Console;
 
 public static class Program
 {
-    public static async Task Initialize(string lobbyCode)
-    {
-        await UdpTransport.Initialize(lobbyCode);
-        Input.Poll();
-    }
-
     public static async Task Main(string[] args)
     {
         var lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
-        await Initialize(lobbyCode);
-        /*
-        AnsiConsole.WriteLine($"Address: {ip}:{port}");
-        AnsiConsole.WriteLine($"encoded blob: {IpPortEncoder.Encode(ip, port)}");
-        AnsiConsole.WriteLine(
-            $"decoded blob: {IpPortEncoder.Decode(IpPortEncoder.Encode(ip, port))}"
-        );
-        AnsiConsole.WriteLine($"Player id: {playerId}");
-        */
-
-        //await Lobby.Join("1234", playerId, ip, port);
+        await UdpTransport.Initialize(lobbyCode);
     }
 
     // placeholder ig
---
+++ src/UI/Messages.cs
@@ -0,0 +1,12 @@
+namespace ATMR.UI;
+
+using Spectre.Console;
+
+public class Messages()
+{
+    public void Initialize()
+    {
+        var panel = new Panel("Hello World");
+        panel.Expand = true;
+    }
+}
