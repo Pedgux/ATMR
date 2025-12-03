390de437e1f89135ca1c0e701134b6b06381f8cd
Eelis Askolin <gr301843@gradia.fi>
2025-12-03 10:41:43 +0200

    inputtia

--- src/Program.cs
+++ src/Program.cs
@@ -4,15 +4,22 @@ Inspired by Nathan Daniel's "Roguelike Theory of Relativity (RTOR)" paper
 Built with Spectre.Console for console UI: https://spectreconsole.net/
 */
 
+using System.Threading.Tasks;
 using ATMR.Networking;
 using Spectre.Console;
 
 public static class Program
 {
+    public static async Task Initialize(string lobbyCode)
+    {
+        await UdpTransport.Initialize(lobbyCode);
+        Input.Poll();
+    }
+
     public static async Task Main(string[] args)
     {
         var lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
-        await UdpTransport.Initialize(lobbyCode);
+        await Initialize(lobbyCode);
         /*
         AnsiConsole.WriteLine($"Address: {ip}:{port}");
         AnsiConsole.WriteLine($"encoded blob: {IpPortEncoder.Encode(ip, port)}");
