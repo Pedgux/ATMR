f846be49fa2123418d02958d43fad5cbe1e96448
Pedgux <matiaslehtinen08@gmail.com>
2025-12-14 00:51:35 +0200

    documentation for class

--- src/Game/Level.cs
+++ src/Game/Level.cs
@@ -3,10 +3,12 @@ namespace ATMR.Level;
 using Arch.Core;
 using ATMR.Components;
 
+/// <summary>
+/// Holds a world with it's entities and a level identifier
+/// </summary>
 public class Level
 {
     private World _world;
-
     public int LevelNumber { get; private set; }
 
     public Level(int levelNumber)
