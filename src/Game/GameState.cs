9ecfdf5bf1df9d74cb9449823e3cf661f8c1740e
Pedgux <matiaslehtinen08@gmail.com>
2025-12-14 01:00:39 +0200

    adjust right size of UI

--- src/Game/GameState.cs
+++ src/Game/GameState.cs
@@ -7,16 +7,22 @@ namespace ATMR;
 /// </summary>
 public static class GameState
 {
+    // UI stuff
     public static UI.UI Ui { get; set; } = null!;
     public static UI.Messages MessageWindow { get; set; } = null!;
     public static UI.Stats StatsWindow { get; set; } = null!;
+
+    // Console stuff
     public static int ConsoleWidth = Console.WindowWidth;
     public static int ConsoleHeight = Console.WindowHeight;
 
     // 6/10 of the Console
     public static int leftSize = (int)Math.Floor(ConsoleWidth / 10.0 * 6);
 
-    // 4/10 of the Console
-    public static int rightSize = (int)Math.Floor(ConsoleWidth / 10.0 * 4);
+    // 4/10 of the Console.
+    // probably some weird rounding error, but this leaves out 1 tile, hence the +1. Investigate later
+    public static int rightSize = (int)Math.Floor(ConsoleWidth / 10.0 * 4) + 1;
+
+    // holds pings. wow.
     public static List<long> PingList = [];
 }
