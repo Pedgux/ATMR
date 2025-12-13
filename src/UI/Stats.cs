1ebdcedc9ffc42b2d7e370bc326cd388d22a28af
Pedgux <matiaslehtinen08@gmail.com>
2025-12-13 22:28:39 +0200

    StatEntries idea

--- src/UI/Stats.cs
+++ src/UI/Stats.cs
@@ -13,6 +13,14 @@ public sealed class Stats
     private Panel _statsPanel;
     private readonly Layout _statsWindow;
 
+    // work in progress, idea is to later put strings there like:
+    // $"str: {int}", or ping for example $"ping: {int} ms"
+    // in a dynamic way, so it's automatically slotted onto the stats panel.
+    // Instead of manually putting them in the code, allowing more customization and eases the whole thing.
+    // But uhh future me problem, don't even have stats yet. Just ideas.
+    // also gotta figure out how to detach the {var} part from the string.
+    public static List<string> StatEntries = [];
+
     // Constructor: capture the layout child. Live will be started at program-level.
     public Stats()
     {
