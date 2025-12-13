05db4f03f1cabf6589adeb9ba8c0fcacb6bfc40e
Pedgux <matiaslehtinen08@gmail.com>
2025-12-13 22:28:53 +0200

    make multiplayer optional

--- src/Program.cs
+++ src/Program.cs
@@ -14,32 +14,53 @@ using Spectre.Console;
 
 public static class Program
 {
-    public static async Task Initialize(string lobbyCode)
+    public static async Task InitializeNetworking(string lobbyCode)
     {
-        // Start input polling and consumer so key events are handled during init
-        var cts = new CancellationTokenSource();
-        var reader = Input.StartPolling(cts.Token);
-        _ = Input.RunConsumer(reader, cts.Token);
-
         await UdpTransport.Initialize(lobbyCode);
     }
 
     public static async Task Main()
     {
-        var lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
+        string lobbyCode = "";
+
+        var multiplayer = AnsiConsole.Prompt(
+            new TextPrompt<string>("Would you like to play multiplayer? (y/n): ")
+        );
+
+        // prompt the user for lobby code, later merge this into the other one below, when prompting works.
+        // Input reader is affecting this way of prompting in some weird way, hence it's below. Fix later.
+        if (multiplayer == "y")
+        {
+            lobbyCode = AnsiConsole.Prompt(new TextPrompt<string>("Type out a lobby code: "));
+        }
+
+        // Start input polling and consumer
+        var cts = new CancellationTokenSource();
+        var reader = Input.StartPolling(cts.Token);
+        _ = Input.RunConsumer(reader, cts.Token);
+
+        // clear the console, so UI fits
         AnsiConsole.Clear();
 
+        // Instantiate all UI parts
         GameState.Ui = new UI();
-
-        // Create and register the messages panel (no live started here)
         GameState.MessageWindow = new Messages();
-
         GameState.StatsWindow = new Stats();
 
-        // Start networking initialization in background so UI Live runs immediately
-        _ = Initialize(lobbyCode);
+        // optional multiplayer
+        if (multiplayer == "y")
+        {
+            // Start networking initialization in background, important to be after UI initialization
+            // because how tf do I prompt in messagewindow? Network init needs UI,
+            // prompting user does not. This is messy.
+            // todo: idk a prompt window? Prompt in messagewindow so we can init UI at start.
+            _ = InitializeNetworking(lobbyCode);
+        }
 
         // Start one Live session bound to the root layout and refresh messages inside it.
+        // Reminder: maybe do a signal based version of this? Instead of polling evert 60ms, what if
+        // UI updates were done when needed? Very inefficient currently.
+        // todo: idk put this elsewhere? Such as in UI.Initialize? Weird to be here.
         await AnsiConsole
             .Live(GameState.Ui.RootLayout)
             .StartAsync(async ctx =>
