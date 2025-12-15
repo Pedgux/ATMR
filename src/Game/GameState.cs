9c78d796ec8b1ee68e4c93f0ef4a862664afcd36
Pedgux <matiaslehtinen08@gmail.com>
2025-12-15 11:03:00 +0200

    calculations for Ui sizes

--- src/Game/GameState.cs
+++ src/Game/GameState.cs
@@ -9,17 +9,25 @@ public static class GameState
     public static UI.UI Ui { get; set; } = null!;
     public static UI.Messages MessageWindow { get; set; } = null!;
     public static UI.Stats StatsWindow { get; set; } = null!;
+    public static UI.Grid GridWindow { get; set; } = null!;
 
     // Console stuff
     public static int ConsoleWidth = Console.WindowWidth;
     public static int ConsoleHeight = Console.WindowHeight;
 
     // 6/10 of the Console
-    public static int leftSize = (int)Math.Floor(ConsoleWidth / 10.0 * 6);
+    public static int LeftWidth = (int)Math.Floor(ConsoleWidth / 10.0 * 6);
 
-    // 4/10 of the Console.
+    // 4/10 of the Console
     // probably some weird rounding error, but this leaves out 1 tile, hence the +1. Investigate later
-    public static int rightSize = (int)Math.Floor(ConsoleWidth / 10.0 * 4) + 1;
+    public static int RightWidth = (int)Math.Floor(ConsoleWidth / 10.0 * 4) + 1;
+
+    // 2/10 of the Console
+    // more weird rounding raah
+    public static int LeftBottom = (int)Math.Floor(ConsoleHeight / 10.0 * 2) + 1;
+
+    // 8/10 of the Console
+    public static int LeftTop = (int)Math.Floor(ConsoleHeight / 10.0 * 8);
 
     // holds pings. wow.
     public static List<long> PingList = [];
