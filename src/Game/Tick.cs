ab9be31f39d7bad04fe5f76e609085fac1529f62
Teletuppi <eltsufi@gmail.com>
2025-12-18 18:27:17 +0200

    input tsydäämiä

--- src/Game/Tick.cs
+++ src/Game/Tick.cs
@@ -5,16 +5,16 @@ using ATMR.Game;
 using ATMR.Systems;
 using Microsoft.VisualBasic;
 
-class Tick
+public class Tick
 {
     private Tick() { }
 
     // call each system in order with player inputs
-    public static async Task<Tick> CreateAsync(Dictionary<int, ConsoleKey> input, Level level)
+    public static async Task<Tick> CreateAsync(Dictionary<int, ConsoleKeyInfo> input, Level level)
     {
         var turn = new Tick();
         // await so they fully execute before another starts
-        //await InputSystem.Run(input, level.World)
+        await InputSystem.Run(level.World, input);
         await RenderSystem.Run(level.World);
         await MovementSystem.Run(level.World);
         return turn;
