a1ab842cceb66104a5324926ad5c6b01adf6fc63
Pedgux <matiaslehtinen08@gmail.com>
2025-12-20 21:33:29 +0200

    cleanup

--- src/ECS/Systems/MovementSystem.cs
+++ src/ECS/Systems/MovementSystem.cs
@@ -10,21 +10,22 @@ public static class MovementSystem
     public static async Task Run(World world)
     {
         var movables = new QueryDescription().WithAll<Position, Velocity>();
-        var obstacles = new QueryDescription().WithAll<Position, Solid>();
+        //var obstacles = new QueryDescription().WithAll<Position, Solid>();
 
         world.Query(
             in movables,
             (Entity entity, ref Position pos, ref Velocity vel) =>
             {
+                // replace the last cell the entity was in, so no duplicates appear
                 if (vel.X != 0 || vel.Y != 0)
                 {
                     GameState.GridWindow.SetGridCell(pos.X, pos.Y, " ");
                 }
 
-                // move the entity to Velocity position
+                // move the entity with velocity
                 pos.X += vel.X;
                 pos.Y += vel.Y;
-
+                // reset velocity
                 vel.X = 0;
                 vel.Y = 0;
             }
