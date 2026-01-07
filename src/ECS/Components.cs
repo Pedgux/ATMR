a27f753970fe508e72e87df899d28f1adea7156b
Pedgux <matiaslehtinen08@gmail.com>
2026-01-07 15:28:59 +0200

    Camera first commit

--- src/ECS/Components.cs
+++ src/ECS/Components.cs
@@ -1,3 +1,5 @@
+using System.Dynamic;
+
 namespace ATMR.Components;
 
 public record struct Position(int X, int Y);
@@ -11,3 +13,34 @@ public record struct Velocity(int X, int Y);
 public record struct Player(int ID);
 
 public record struct Solid();
+
+/// <summary>
+/// A simple view to the game world.
+/// </summary>
+public record struct Camera(int X, int Y)
+{
+    public int LeftTop { get; set; }
+    public int LeftBottom { get; set; }
+    public int RightTop { get; set; }
+    public int RightBottom { get; set; }
+
+    public Camera(int x, int y, int Width, int Height)
+        : this(x, y)
+    {
+        X = x;
+        Y = y;
+
+        // calculate halfs
+        int firstWidthHalf = (int)Math.Floor(Width / 2.0);
+        int secondWidthHalf = Width - firstWidthHalf;
+
+        int firstHeightHalf = (int)Math.Floor(Height / 2.0);
+        int secondHeightHalf = Height - firstHeightHalf;
+
+        // Assign camera corners
+        LeftTop = x - firstWidthHalf;
+        LeftBottom = y - firstHeightHalf;
+        RightTop = x + secondWidthHalf;
+        RightBottom = y + secondHeightHalf;
+    }
+}
