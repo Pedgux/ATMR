bfab4ed85fc5cfaf5a1c9be5e0d08b246dd4e43d
Eelis Askolin <gr301843@gradia.fi>
2026-01-20 11:53:46 +0200

    jebajei inputtia, conpaktoitu ja sillee yk

--- src/ECS/Systems/InputSystem.cs
+++ src/ECS/Systems/InputSystem.cs
@@ -1,6 +1,7 @@
 using Arch.Core;
 using ATMR.Components;
 using ATMR.Game;
+using ATMR.Helpers;
 using Spectre.Console;
 
 namespace ATMR.Systems;
@@ -10,66 +11,31 @@ public static class InputSystem
     // eli siis itse inputtien toiminnot.
     public static async Task Run(World world, Dictionary<int, ConsoleKeyInfo> inputs)
     {
-        Entity player = GameState.Player1;
-
         var query = new QueryDescription().WithAll<Player>();
         world.Query(
             in query,
             (Entity entity, ref Player player) =>
             {
                 foreach (var kvp in inputs)
-                {
-                    ref var velocity = ref world.Get<Velocity>(entity);
                     if (player.ID == kvp.Key)
                     {
-                        switch (kvp.Value.Key)
-                        {
-                            case ConsoleKey.UpArrow:
-                                velocity.Y -= 1;
-                                break;
-                            case ConsoleKey.DownArrow:
-                                velocity.Y += 1;
-                                break;
-                            case ConsoleKey.LeftArrow:
-                                velocity.X -= 1;
-                                break;
-                            case ConsoleKey.RightArrow:
-                                velocity.X += 1;
-                                break;
-                            case ConsoleKey.H:
-                                velocity.X -= 1;
-                                break;
-                            case ConsoleKey.J:
-                                velocity.Y += 1;
-                                break;
-                            case ConsoleKey.K:
-                                velocity.Y -= 1;
-                                break;
-                            case ConsoleKey.L:
-                                velocity.X += 1;
-                                break;
-                            case ConsoleKey.Y:
-                                velocity.X -= 1;
-                                velocity.Y -= 1;
-                                break;
-                            case ConsoleKey.U:
-                                velocity.X += 1;
-                                velocity.Y -= 1;
-                                break;
-                            case ConsoleKey.B:
-                                velocity.X -= 1;
-                                velocity.Y += 1;
-                                break;
-                            case ConsoleKey.N:
-                                velocity.X += 1;
-                                velocity.Y += 1;
-                                break;
+                        ref var velocity = ref world.Get<Velocity>(entity);
 
-                            default:
-                                break;
-                        }
+                        (int dx, int dy) = Keybinds.GetActionWithKey(kvp.Value.Key) switch
+                        {
+                            "2" => (0, 1),
+                            "4" => (-1, 0),
+                            "6" => (1, 0),
+                            "8" => (0, -1),
+                            "1" => (-1, 1),
+                            "3" => (1, 1),
+                            "7" => (-1, -1),
+                            "9" => (1, -1),
+                            _ => (0, 0),
+                        };
+                        velocity.X += dx;
+                        velocity.Y += dy;
                     }
-                }
             }
         );
     }
