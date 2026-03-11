6d06e0ee598063df108a1ee041df2ac950976b6a
Pedgux <matiaslehtinen08@gmail.com>
2026-03-11 08:22:35 +0200

    debug print

--- src/ECS/Systems/FollowSystem.cs
+++ src/ECS/Systems/FollowSystem.cs
@@ -1,5 +1,6 @@
 using Arch.Core;
 using ATMR.Components;
+using ATMR.Game;
 
 namespace ATMR.Systems;
 
@@ -14,6 +15,9 @@ public static class FollowSystem
             (Entity entity, ref Position position, ref FollowsEntity follows) =>
             {
                 var targetPos = world.Get<Position>(follows.Target);
+                GameState.MessageWindow.Write(
+                    $"[blue]Followed from ({position.X}, {position.Y} to ({targetPos.X}, {targetPos.Y}))[/]"
+                );
                 position.X = targetPos.X;
                 position.Y = targetPos.Y;
             }
