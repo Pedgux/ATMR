6412be33e17919c3cdc0e10c27fee956a0143a1f
Pedgux <matiaslehtinen08@gmail.com>
2026-03-19 09:20:34 +0200

    kill

---
+++ src/ECS/Systems/HealthSystem.cs
@@ -0,0 +1,24 @@
+using Arch.Core;
+using Arch.Core.Extensions;
+using ATMR.Components;
+
+namespace ATMR.Systems;
+
+public static class HealthSystem
+{
+    public static void Run(World world)
+    {
+        var query = new QueryDescription().WithAll<Health>();
+
+        world.Query(
+            in query,
+            (Entity entity, ref Health health) =>
+            {
+                if (health.Amount < 0)
+                {
+                    entity.Add(new Destroy());
+                }
+            }
+        );
+    }
+}
