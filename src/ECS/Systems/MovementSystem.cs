8ed9b206e95d274c8a7a7d651c5b83e92ac1b0df
Pedgux <matiaslehtinen08@gmail.com>
2026-09-02 13:05:03 +0300

    Remove Teleportsystem from movement system into own system

--- src/ECS/Systems/MovementSystem.cs
+++ src/ECS/Systems/MovementSystem.cs
@@ -5,14 +5,13 @@ using ATMR.Game;
 namespace ATMR.Systems;
 
 /// <summary>
-/// Moves entities either by a velocity or a teleport.
+/// Moves entities.
 /// </summary>
 public static class MovementSystem
 {
     public static void Run(World world)
     {
         var movables = new QueryDescription().WithAll<Position, Velocity>();
-        var teleportables = new QueryDescription().WithAll<Position, Teleport>();
 
         world.Query(
             in movables,
@@ -47,37 +46,6 @@ public static class MovementSystem
                 // reset velocity
                 vel.X = 0;
                 vel.Y = 0;
-            }
-        );
-
-        world.Query(
-            in teleportables,
-            (Entity entity, ref Position pos, ref Teleport tp) =>
-            {
-                if (tp.X == 0 && tp.Y == 0)
-                    return;
-
-                bool isSolid = world.Has<Position, Solid>(entity);
-                bool canTeleport = isSolid
-                    ? CollisionSystem.TryMoveSolid(entity, pos.X, pos.Y, tp.X, tp.Y)
-                    : !CollisionSystem.IsBlocked(tp.X, tp.Y);
-
-                if (!canTeleport)
-                {
-                    tp.X = 0;
-                    tp.Y = 0;
-                    return;
-                }
-
-                // replace the last cell the entity was in, so no duplicates appear
-                GameState.GridWindow.RestoreBaseTile(pos.X, pos.Y);
-
-                // teleport em
-                pos.X = tp.X;
-                pos.Y = tp.Y;
-
-                tp.X = 0;
-                tp.Y = 0;
             }
         );
     }
