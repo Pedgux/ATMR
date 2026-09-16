729b99b8a1fc18fe30437846734bd36c712c78f5
Teletuppi <eltsufi@gmail.com>
2026-09-16 14:44:07 +0300

    juttui

---
+++ src/ECS/Systems/AttackSystem.cs
@@ -0,0 +1,41 @@
+using Arch.Core;
+using ATMR.Components;
+
+namespace ATMR.Systems;
+
+public static class AttackSystem
+{
+    public static void Run(World world)
+    {
+        /*
+        if (intents.Count == 0)
+        {
+            return;
+        }
+
+        var digIntents = intents
+            .Where(intent => intent.Kind == ActionKind.Dig)
+            .GroupBy(intent => intent.PlayerId)
+            .ToDictionary(group => group.Key, group => group.Last());
+
+        if (digIntents.Count == 0)
+        {
+            return;
+        }
+        */
+        var query = new QueryDescription().WithAll<AttackIntent, Position>();
+
+        world.Query(
+            in query,
+            (Entity entity, ref Position position, ref AttackIntent intent) =>
+            {
+                int targetX = position.X + intent.X;
+                int targetY = position.Y + intent.Y;
+
+                SpatialGrid.TryGetEntity(targetX, targetY, out Entity targetEntity);
+                // ExecuteDig(world, targetX, targetY);
+                //GameState.TimeCounter += 20;
+            }
+        );
+    }
+}
